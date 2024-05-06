c78c626879ec909a52b925938cf503e345d0d771
Slendergo <rusjaellgo@gmail.com>
2024-05-06 03:17:16 +0100

    Services
    What each service type is
    
    Singleton
    Scoped
    Transient

---
+++ Services/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Services.Services;
+
+namespace EntityFramework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Only one service instance across application lifetime
+            builder.Services.AddSingleton<IServiceTest, FirstService>();
+
+            // New Instance every time its requested
+            //builder.Services.AddTransient<IServiceTest, FirstService>();
+
+            // Single Service across the entire instance request
+            //builder.Services.AddScoped<IServiceTest, FirstService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Services/Services/IServiceTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Services.Services
+{
+    public interface IServiceTest
+    {
+        public void PrintTypeName();
+        public void LogMessage(string message);
+    }
+
+    public sealed class FirstService : IServiceTest
+    {
+        private readonly ILogger<FirstService> _logger;  
+
+        public FirstService(ILogger<FirstService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void PrintTypeName()
+        {
+            LogMessage($"{GetType().Name}");
+        }
+
+        public void LogMessage(string message)
+        {
+            _logger.LogInformation(message);
+        }
+    }
+}
